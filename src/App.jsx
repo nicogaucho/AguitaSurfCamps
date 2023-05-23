ba23f3f8a877870eef0efc40dd5a22b0ba15002c
Nicola Gasparro <nicola.gasp11@gmail.com>
2023-05-23 11:48:55 +0100

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Surf from './pages/Surf';
 import Accomodation from './pages/Accomodation';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/surfing' element={<Surf />} />
